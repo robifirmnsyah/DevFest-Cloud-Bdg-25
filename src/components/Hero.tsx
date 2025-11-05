8cea3def12409060b6579b60af8061bf50a5ecda
Robi Firmansyah <robifirmansyah314@gmail.com>
2025-11-05 09:49:14 +0700

    Refactor code structure for improved readability and maintainability

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -61,8 +61,8 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      {/* Scroll Indicator - Hidden on mobile */}
+      <div className="hidden md:block absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center">
           <div className="w-1.5 h-3 bg-white/70 rounded-full mt-2"></div>
         </div>
